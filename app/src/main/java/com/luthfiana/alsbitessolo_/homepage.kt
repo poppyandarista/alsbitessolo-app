0f5eeb125b5a4f966ebcfd780d77d68986689e58
poppyandarista <andarista999@gmail.com>
2025-01-20 13:56:40 +0700

    added homepage

--- app/src/main/java/com/luthfiana/alsbitessolo_/homepage.kt
+++ app/src/main/java/com/luthfiana/alsbitessolo_/homepage.kt
@@ -1,29 +1,20 @@
 package com.luthfiana.alsbitessolo_
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_homepage)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val getStartedButton: Button = findViewById(R.id.getStartedButton)
-        getStartedButton.setOnClickListener {
-            val intent = Intent(this, homepage::class.java)
-            startActivity(intent)
         }
     }
 }
